0d017eaf4889dae7188812e49edfb498b233982c
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-08 23:01:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/grojec/VideActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logonrmlocal.grojec
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vide)
+    }
+}
